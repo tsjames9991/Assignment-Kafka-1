a12a799ece2cf57b63de7e9082a15f362212e91e
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-03-08 10:00:35 +0530

    Completed Assignment

--- src/main/scala/serde/package.scala
+++ src/main/scala/serde/package.scala
@@ -3,11 +3,12 @@ import org.apache.log4j.Logger
 package object serde {
   val ROLLNUMBER = 50
   val student: Student = Student(ROLLNUMBER, "Sudeep James Tirkey")
-val TIMEOUT = 5000
+  val TIMEOUT = 5000
+  val Log = Logger.getLogger(this.getClass)
+
   case class Student(id: Int, name: String) {
     override def toString: String = {
       s"$id : $name"
     }
   }
-  val Log = Logger.getLogger(this.getClass)
 }
